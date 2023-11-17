ce3f5217981c5a495e0b9cb26fce1933cd953be2
takahiro <linlonghao61@gmail.com>
2023-11-17 23:23:33 +0900

    htmlにコメント追加

--- src/main/java/com/example/demo/trySpring/HelloController.java
+++ src/main/java/com/example/demo/trySpring/HelloController.java
@@ -5,8 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
  // ポイント１：@Controller
 @Controller
 public class HelloController {
+ // ポイント１：@Controller
 @GetMapping("/hello")
   public String getHello() {
+ //hello.htmlに画面遷移
  return "hello";
  }
 }
