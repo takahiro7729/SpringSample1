d6a0da2c0352973b32035705de28f83eb10bc6e6
takahiro <linlonghao61@gmail.com>
2023-11-17 22:34:21 +0900

    Spring FWを使用してHello Worldを表示

---
+++ src/main/java/com/example/demo/SpringSample1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSample1Application.class, args);
+	}
+
+}
