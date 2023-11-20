872b7f44df695702dfe99e3672360d1c54f16c0b
takahiro <linlonghao61@gmail.com>
2023-11-20 18:27:54 +0900

    画面から値を渡してみる

--- src/main/java/com/example/demo/trySpring/HelloController.java
+++ src/main/java/com/example/demo/trySpring/HelloController.java
@@ -1,7 +1,11 @@
 package com.example.demo.trySpring;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
  // ポイント１：@Controller
 @Controller
 public class HelloController {
@@ -10,6 +14,15 @@ public class HelloController {
   public String getHello() {
  //hello.htmlに画面遷移
  return "hello";
- }
 }
-
+// ポイント１：@PostMapping
+// ポイント２：@RequestParam
+@PostMapping("/hello")
+public String postRequest(@RequestParam("text1")String str,
+		Model model) {
+    //画面から受け取った文字列をmodelに登録
+	model.addAttribute("sample",str);
+	//helloResponse。htmlに画面遷移
+	return "helloResponse";
+	}
+}
